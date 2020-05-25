374c6c001060ffef0a8ded7f7e9f393d7ce818b0
Josh Martin <denverprogrammer@gmail.com>
2020-05-24 19:32:06 -0600

    SECURITY:#COMMENT Fixed psr errors.

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\Security\Core\Security;
 
 /**
@@ -23,7 +22,7 @@ class SecurityController extends AbstractController
      *
      * @Route("/profile", name="profile")
      */
-    public function profile(Security $security) : JsonResponse
+    public function profile(Security $security): JsonResponse
     {
         $user = $security->getUser();
 
